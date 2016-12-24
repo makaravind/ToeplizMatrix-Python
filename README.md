222668aac80f5535c27764839d356e938f1018a2
aravind <makaravind>
2016-12-24 11:47:14 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# ToeplizMatrix-Python
+
+A Toeplitz matrix or diagonal-constant matrix. This is a python script to check whether the given 
+matrix is a Toepliz matrix or not.
---
+++ ToeplizMatrix.py
@@ -0,0 +1,34 @@
+# representing in 1D for better search and performance
+input = [6, 7, 8, 9, 2, 4, 6, 7, 8, 9, 1, 4, 6, 7, 8, 0, 1, 4, 6, 7]
+# input = [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6]
+# input = [6]
+
+# assuming a sqmat always
+size = 5
+
+def isToeplizMatrix(input_matrix, size):
+
+    #initializing with first row
+    init_pattern = input[:size - 1]
+    observed_pattern = []
+
+    #looping through row wise in 1D list
+    for i in range(size, len(input), size):
+        #print(input[i: i+size])
+        observed_pattern = input[i: i + size] #cosidering all the elements in the row
+        to_append_ele = observed_pattern[0] #taking the first ele away from the pattern
+        observed_pattern = observed_pattern[1:]
+        if init_pattern == observed_pattern: #checking if the toepliz nature holds
+            observed_pattern.insert(0, to_append_ele)
+            init_pattern = observed_pattern[:len(observed_pattern)-1] #reading the init_pattern for next round
+        else:
+            return False
+    return True
+
+
+if(isToeplizMatrix(input, size)):
+    print("the given input is toepliz matrix")
+else:
+    print("the given input is not toepliz matrix")
+
+
